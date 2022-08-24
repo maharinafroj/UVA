a59c55fd16cf6b846e0e4e7dd6c2a0effa0cef44
maharin_afroj <74603459+maharinafroj@users.noreply.github.com>
2022-08-25 01:18:41 +0600

    Create 12646 - Zero or One.cpp

---
+++ 12646 - Zero or One.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    while (cin >> a >> b >> c)
+    {
+        if (a == b && b == c)
+            cout << "*" << endl;
+        else if (a == b)
+            cout << "C" << endl;
+        else if (a == c)
+            cout << "B" << endl;
+        else
+            cout << "A" << endl;
+    }
+}
